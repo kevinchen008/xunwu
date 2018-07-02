7b21007c3093db18d4101c0d4943f5927ac3c157
lucx <715203709@qq.com>
2018-07-02 22:30:20 +0800

    up

--- src/main/java/com/zku/xunwu/XunwuApplication.java
+++ src/main/java/com/zku/xunwu/XunwuApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XunwuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XunwuApplication.class, args);
+		              SpringApplication.run(XunwuApplication.class, args);
 	}
 }
